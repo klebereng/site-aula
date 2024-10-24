cb79b5cef91b88493a09da391a5693666ea1c321
Kleber Machado de Oliveira Junior <kmbj321@gmail.com>
2024-10-23 22:08:48 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -18,7 +18,7 @@ import streamlit as st
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("A cat")
+    st.header("Otavio Morale Faculdade")
     st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt-br.facebook.com%2Fgroups%2F100616670283255%2Fpermalink%2F628679570810293%2F&psig=AOvVaw10O-UcDWR4Xq_DXBFCNApn&ust=1729818440501000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCdiv3ppYkDFQAAAAAdAAAAABAE")
 
 with col2:
